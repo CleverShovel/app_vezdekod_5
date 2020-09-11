09740c7b41fe30ffada44f0be8a75a1da1c2ef46
CleverShovel <vladimirpanov73@gmail.com>
2020-09-11 17:37:25 +0400

    first commit

---
+++ app/src/main/java/com/example/app_vezdekod_5/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.app_vezdekod_5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onCreateFundClick(View v) {
+        Intent questionIntent = new Intent(MainActivity.this,
+                ChooseFundingType.class);
+        startActivity(questionIntent);
+    }
+}
